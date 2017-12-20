2638534626b62fc93b38fc681a82829c54451efc
Felix <x@yg.ht>
2017-12-20 17:56:24 +0000

    Early comits - all boring, just building functionality

--- FIR.py
+++ FIR.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
-import sys
-import os
-import optparse
+#app modules below
 import settings
 import definitions
 import functions
 
-parser = optparse.OptionParser(usage='./%prog -N CIDR_mask', version=settings.__version__, prog=sys.argv[0])
-parser.add_option('-N', '--network', action="store", help="Specify the target network.", dest="targetNetwork", default="127.0.0.1/32")
-options, args = parser.parse_args()
+#python modules below
+import sys
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser(description='FIR - Fast Initial Recon, a tool to extract details about a given network in a way that is efficient for penetration testing', version=settings.__version__)
+parser.add_argument("-N", "--targetNetwork", action="store", default="127.0.0.1/32", help="Specify the target network in CIDR notaton.", dest="targetNetwork")
+args = parser.parse_args()
 
 if not os.geteuid() == 0:
     print("[!] Must be run as root.")
