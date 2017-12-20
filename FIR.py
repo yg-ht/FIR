abc02fa6c4fd5076ec1ed0dc857a151ac9a6d1c5
Felix <x@yg.ht>
2017-12-20 16:32:42 +0000

    Early comits - all boring, just building functionality

--- FIR.py
+++ FIR.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 import optparse
 import settings
 import definitions
 import functions
 
 parser = optparse.OptionParser(usage='./%prog -N CIDR_mask', version=settings.__version__, prog=sys.argv[0])
-parser.add_option('-N', '--network', action="store", help="Specify the target network.", dest="targetNetwork")
+parser.add_option('-N', '--network', action="store", help="Specify the target network.", dest="targetNetwork", default="127.0.0.1/32")
 options, args = parser.parse_args()
+
+if not os.geteuid() == 0:
+    print("[!] Must be run as root.")
+sys.exit(-1)
 
 functions.buildmainarrays(args.targetNetwork)
 functions.portScan('127.0.0.1', 8000, settings.nmapGenericSettings)
