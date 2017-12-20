b1fce32e94a9779e45788b5290624e719422df52
Felix <x@yg.ht>
2017-12-20 17:57:59 +0000

    Early comits - all boring, just building functionality

--- FIR.py
+++ FIR.py
@@ -11,8 +11,9 @@ import os
 import argparse
 
 
-parser = argparse.ArgumentParser(description='FIR - Fast Initial Recon, a tool to extract details about a given network in a way that is efficient for penetration testing', version=settings.__version__)
+parser = argparse.ArgumentParser(description='FIR - Fast Initial Recon, a tool to extract details about a given network in a way that is efficient for penetration testing')
 parser.add_argument("-N", "--targetNetwork", action="store", default="127.0.0.1/32", help="Specify the target network in CIDR notaton.", dest="targetNetwork")
+parser.add_argument("-V", '--version', action='version', version=settings.__version__)
 args = parser.parse_args()
 
 if not os.geteuid() == 0:
