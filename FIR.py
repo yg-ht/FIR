b0c3f258b0d45f71d6b46f940ac56af3e90de30e
Felix <x@yg.ht>
2017-12-20 16:24:15 +0000

    Early comits - all boring, just building functionality

--- FIR.py
+++ FIR.py
@@ -3,6 +3,7 @@
 import sys
 import optparse
 import settings
+import definitions
 import functions
 
 parser = optparse.OptionParser(usage='./%prog -N CIDR_mask', version=settings.__version__, prog=sys.argv[0])
@@ -10,4 +11,4 @@ parser.add_option('-N', '--network', action="store", help="Specify the target ne
 options, args = parser.parse_args()
 
 functions.buildmainarrays(args.targetNetwork)
-
+functions.portScan('127.0.0.1', 8000, settings.nmapGenericSettings)
