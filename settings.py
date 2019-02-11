d14f5675d00b54d36da0929feb2ec50009467cc2
YGHT <yg-ht@users.noreply.github.com>
2019-02-11 21:56:34 +0000

    Update settings.py
    Added time out setting for DNS tools

--- settings.py
+++ settings.py
@@ -15,3 +15,5 @@ portsForScanning_TCP = [21, 22, 23, 25, 53, 79, 80, 138, 139, 389, 443, 445, 512
                         5900, 8000, 8080]
 # portsForScanning_TCP = [139,445] # test limiter
 portsForScanning_UDP = [53, 161, 162]
+
+dnsTimeoutSeconds = 1
